180f2eec502de32f3eccd2e2bd9389774a568e57
zli <1036281414@qq.com>
2023-11-27 13:45:28 +0800

    register iMAPExeBatch

--- gym_eda/__init__.py
+++ gym_eda/__init__.py
@@ -5,6 +5,8 @@ from gym_eda.cirkit_env_v1 import CirkitEnv, CirkitEnv_step
 from gym_eda.abc_env_asic import AbcEnv_ASIC, AbcEnv_ASIC_step
 from gym_eda.imap_env import iMAPEnv
 from gym_eda.imap_exe import iMAPExe
+from gym_eda.imap_exe_batch import iMAPExeBatch
+
 
 from gym.envs.registration import register
 
